3ceace6216ddef57f963848b9262159261967db7
TASKIN AHMAD AL FARUQE <119452412+taskinahmadalfaruqe@users.noreply.github.com>
2023-07-21 16:57:19 +0600

    Navbar Responsive Done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+let bar = document.querySelector(".bar");
+let list = document.querySelector(".list");
+
+    bar.addEventListener('click' , ()=>{
+        list.classList.toggle("active")
+    });
+    
